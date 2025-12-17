95094a6527120143d0e5dfd6aba3257c7fb0db44
Maduki-tech <d.schlueter1011@gmail.com>
2025-12-17 11:25:44 +0100

    Created a Simple port scanner

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	server := http.Server{
+		Addr: ":8080",
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			fmt.Println("Server error:", err)
+		}
+	}()
+	fmt.Println("Starting port scan on localhost...")
+	for port := 1; port <= 10000; port++ {
+		if ScanPort(port) {
+			fmt.Printf("Port %d is open\n", port)
+		}
+	}
+}
+
+func ScanPort(port int) bool {
+	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
 }
